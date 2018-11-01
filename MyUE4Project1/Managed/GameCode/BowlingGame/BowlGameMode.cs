a096713efd0a4de22184617607baac6d32f6c8ba
Tanner555 <theman989898@gmail.com>
2018-10-31 21:15:26 -0700

    GameMode Now Gets Camera Manager And Bowling Ball References

--- MyUE4Project1/Managed/GameCode/BowlingGame/BowlGameMode.cs
+++ MyUE4Project1/Managed/GameCode/BowlingGame/BowlGameMode.cs
@@ -18,10 +18,63 @@ namespace HelloUSharp
         [UProperty, EditAnywhere, BlueprintReadWrite]
         public int Value12345 { get; set; }
 
+        [UProperty, EditAnywhere, BlueprintReadWrite, Category("Bowling")]
+        public FVector CameraFollowOffset { get; set; }
+
+        [UProperty, EditAnywhere, BlueprintReadWrite, Category("Bowling")]
+        public TSubclassOf<AActor> BowlingBallSubClassReference { get; set; }
+
+        protected APlayerCameraManager myCameraManager = null;
+        protected BowlingBall myBall = null;
+
         protected override void ReceiveBeginPlay_Implementation()
         {
             //base.ReceiveBeginPlay_Implementation();
             //PrintString("Hello From " + GetName(), FLinearColor.Green);
+            myCameraManager = UGameplayStatics.GetPlayerCameraManager(this, 0);
+            if(myCameraManager != null)
+            {
+                //PrintString("Camera Manager is " + myCameraManager.GetName(), FLinearColor.AliceBlue, printToLog:true);
+            }
+            else
+            {
+                PrintString("Couldn't Find Camera Manager", FLinearColor.AliceBlue);
+            }
+        }
+
+        [UFunction, BlueprintCallable]
+        public void SetBallFromBallFindCollection(List<BowlingBall> balls)
+        {
+            if(balls != null && balls.Count > 0 && balls[0] != null)
+            {
+                //PrintString("Setting my ball to: " + balls[0].GetName(), FLinearColor.Green);
+                myBall = balls[0];
+            }
+        }
+
+        /// <summary>
+        /// TSubClassOf Class Is Always Null, If you try to set a uproperty, it's still null.
+        /// Using This Won't Work: if (BowlingBallSubClassReference != null)
+        /// but this will: if (BowlingBallSubClassReference.Value != null)
+        /// </summary>
+        private void BrokenGetBallAttempt()
+        {
+            if (BowlingBallSubClassReference != null)
+            {
+                PrintString("Class: " + BowlingBallSubClassReference.Value.GetName(), FLinearColor.Green, printToLog: true);
+                List<AActor> _balls = new List<AActor>();
+
+                World.GetAllActorsOfClass(BowlingBallSubClassReference, out _balls);
+
+                if (_balls != null && _balls.Count > 0)
+                {
+                    PrintString("Found some balls", FLinearColor.Green, printToLog: true);
+                }
+                else
+                {
+                    PrintString("Couldn't find any balls", FLinearColor.Green, printToLog: true);
+                }
+            }
         }
     }
 }
