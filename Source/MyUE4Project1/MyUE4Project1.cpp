67a2f498d309aeb49d869b752d15ecca0e8e44ad
Tanner555 <theman989898@gmail.com>
2018-11-02 02:01:33 -0700

    Replaced Entire Project With New Smaller Project

--- Source/MyUE4Project1/MyUE4Project1.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "MyUE4Project1.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyUE4Project1, "MyUE4Project1" );
