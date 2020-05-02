250ea94591525d67d8d46c87acf9a6f66ce2754e
Tanner555 <theman989898@gmail.com>
2020-05-02 04:48:16 -0700

    Removed All CS BPs and Old CS Code

---
+++ Managed/UnrealBowlingUSharp.Managed/BowlingGameComponents/UCSBowlHelperComponent.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnrealEngine.Runtime;
+using UnrealEngine.Engine;
+using UnrealEngine.Slate;
+using UnrealEngine;
+using UnrealEngine.GameplayTasks;
+using UnrealEngine.SlateCore;
+using UnrealEngine.NavigationSystem;
+using UnrealEngine.LevelSequence;
+using UnrealEngine.MovieScene;
+using UnrealEngine.TimeManagement;
+
+namespace USharpBowlProject
+{
+    [UClass, Blueprintable, BlueprintType]
+    public class UCSBowlHelperComponent : UActorComponent
+    {
+        #region Fields
+
+        #endregion
+
+        #region UProperties
+
+        #endregion
+
+        #region Overrides
+        public override void Initialize(FObjectInitializer initializer)
+        {
+            
+        }
+        #endregion
+
+        #region UFunctions
+
+        #endregion
+    }
+}
--- Managed/UnrealBowlingUSharp.Managed/StaticClasses/BowlActionMaster.cs
+++ Managed/UnrealBowlingUSharp.Managed/StaticClasses/BowlActionMaster.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Text;
 using UnrealEngine.Runtime;
 
-namespace HelloUSharp
+namespace USharpBowlProject
 {
     [UEnum]
-    public enum EBowlAction : System.Byte
+    public enum EBowlActionCSharp : System.Byte
     {
         Tidy = 0,
         Reset = 1,
@@ -15,41 +15,56 @@ namespace HelloUSharp
         Undefined = 4
     };
 
+    [UEnum]
+    public enum EBowlFrameCSharp : System.Byte
+    {
+        Frame01 = 0,
+        Frame02 = 1,
+        Frame03 = 2,
+        Frame04 = 3,
+        Frame05 = 4,
+        Frame06 = 5,
+        Frame07 = 6,
+        Frame08 = 7,
+        Frame09 = 8,
+        Frame10 = 9
+    }
+
     [UClassIgnore]
     public static class BowlActionMaster
     {
-        public static EBowlAction NextAction(List<int> rolls)
+        public static EBowlActionCSharp NextAction(List<int> rolls)
         {
-            EBowlAction nextAction = EBowlAction.Undefined;
+            EBowlActionCSharp nextAction = EBowlActionCSharp.Undefined;
 
             for (int i = 0; i < rolls.Count; i++)
             { // Step through rolls
 
                 if (i == 20)
                 {
-                    nextAction = EBowlAction.EndGame;
+                    nextAction = EBowlActionCSharp.EndGame;
                 }
                 else if (i >= 18 && rolls[i] == 10)
                 { // Handle last-frame special cases
-                    nextAction = EBowlAction.Reset;
+                    nextAction = EBowlActionCSharp.Reset;
                 }
                 else if (i == 19)
                 {
                     if (rolls[18] == 10 && rolls[19] == 0)
                     {
-                        nextAction = EBowlAction.Tidy;
+                        nextAction = EBowlActionCSharp.Tidy;
                     }
                     else if (rolls[18] + rolls[19] == 10)
                     {
-                        nextAction = EBowlAction.Reset;
+                        nextAction = EBowlActionCSharp.Reset;
                     }
                     else if (rolls[18] + rolls[19] >= 10)
                     {  // Roll 21 awarded
-                        nextAction = EBowlAction.Tidy;
+                        nextAction = EBowlActionCSharp.Tidy;
                     }
                     else
                     {
-                        nextAction = EBowlAction.EndGame;
+                        nextAction = EBowlActionCSharp.EndGame;
                     }
                 }
                 else if (i % 2 == 0)
@@ -57,16 +72,16 @@ namespace HelloUSharp
                     if (rolls[i] == 10)
                     {
                         //rolls.Insert(i, 0); // Insert virtual 0 after strike
-                        nextAction = EBowlAction.EndTurn;
+                        nextAction = EBowlActionCSharp.EndTurn;
                     }
                     else
                     {
-                        nextAction = EBowlAction.Tidy;
+                        nextAction = EBowlActionCSharp.Tidy;
                     }
                 }
                 else
                 { // Second bowl of frame
-                    nextAction = EBowlAction.EndTurn;
+                    nextAction = EBowlActionCSharp.EndTurn;
                 }
             }
 
